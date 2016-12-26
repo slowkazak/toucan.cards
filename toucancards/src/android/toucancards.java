3f944d7e7b8b27bff04c42815923be5eb730bfda
slowkazak <staskuban@ya.ru>
2016-12-27 00:01:02 +0300

    add payment action sceleton, add simple logic for input parameters validation

--- toucancards/src/android/toucancards.java
+++ toucancards/src/android/toucancards.java
@@ -1,16 +1,29 @@
 package ru.slowkazak.toucancards;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import java.util.Iterator;
+import android.net.Uri;
+import java.lang.reflect.Field;
+import android.content.ActivityNotFoundException;
+import android.util.Log;
+import android.os.Bundle;
+
 
 /**
  * This class echoes a string called from JavaScript.
  */
-public class cards extends CordovaPlugin {
+public class toucancards extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -28,12 +41,49 @@ public class cards extends CordovaPlugin {
     private void payment(JSONObject obj, CallbackContext callbackContext) {
        try {
         if (obj != null) {
-                    callbackContext.success(obj.toString());
+Context context=this.cordova.getActivity().getApplicationContext();
+          Intent intent = new Intent("ru.toucan.PAYMENT");
+
+
+
+
+/*
+                    Iterator<String> iter = obj.keys();
+
+                    					while (iter.hasNext()) {
+                    						key = iter.next();
+                    						value = obj.getString(key);
+                    						 intent.putExtra(key, value);
+                    					}
+*/
+     intent.putExtra("PackageName", "com.readyscript.dk.storemanagement");
+            intent.putExtra("SecureCode", "1234");
+
+            intent.putExtra("Amount", 100);
+            intent.putExtra("Description", "23234234");
+             cordova.getActivity().startActivityForResult(intent, 4);
+                    callbackContext.success(obj.getString("Amount"));
                 } else {
                     callbackContext.error("Expected one non-empty string argument.");
                 }
           } catch(Exception e) {
-               callbackContext.error(e);
+
           }
+
+
     }
+
+
+        /**
+         * Обработка резулта
+         *
+         * @param requestCode
+         * @param resultCode
+         * @param data
+         */
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        }
+
 }
--- toucancards/www/toucancards.js
+++ toucancards/www/toucancards.js
@@ -1,8 +1,49 @@
-cordova.define("ru.slowkazak.toucan.cards", function(require, exports, module) {
+cordova.define("ru.slowkazak.toucancards.toucancards", function (require, exports, module) {
     var exec = require('cordova/exec');
 
-    exports.payment = function(arg0, success, error) {
-        exec(success, error, "toucancards", "payment", [arg0]);
+    exports.start = function (act, arg, success, error) {
+        // Если объект с аргументами не пуст
+        if (Object.keys(arg).length) {
+            var accessible_json_params = [
+                {
+                    act: "payment",
+                    params: [
+                        "PackageName",
+                        "SecureCode",
+                        "Amount",
+                        "Description",
+                        "Email",
+                        "Phone",
+                        "ValueAddedTaxRate"
+                    ]
+                }
+            ]
+            accessible_json_params = accessible_json_params.filter(item => item.act === act).pop();
+            console.log(accessible_json_params.params);
+
+            var action = "ru.toucan.PAYMENT";
+//проверяем на наличие лишних свойств, если находим - удаляем
+            for (var itm in arg) {
+                console.log(itm)
+                if (accessible_json_params.params.indexOf(itm)<0) {
+                    delete arg[itm];
+                }
+            }
+
+            exec(success, error, "toucancards", act, [arg]);
+        }
     };
 
 });
+
+/*
+ https://github.com/smart-fin/AndroidApi/tree/master/V3
+ Amount сумма платежа в МДЕ (в копейках) - целочисленный формат
+ Description назначение платежа
+ FullDescription подробное назначение платежа, используется для печати подробной информации об услуге на кассовом чеке (поддержка ФР Штрих-м)
+ ValueAddedTaxRate НДС (в промилях)
+ Email E-mail для отправки терминального чека
+ Phone Номер телефона для отправки терминального чека
+ GetPayInfo переключатель показывающий надо ли возвращать полную информацию о платеже (1-да, 2-нет)
+ FiscalizationFlag Необходимость фискализации (0 - фискализация нужна(значение по-умолчанию), 1-фискализация не нужна)
+ */
